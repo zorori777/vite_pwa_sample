22e70074d680eb4408120638a8a60d429cd396a3
takumi_watanabe <work.takumi@gmail.com>
2021-12-29 16:32:07 +0900

    add: baseオプションを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import {registerSW} from "virtual:pwa-register";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
+
+registerSW();
--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  base: "./",
+  plugins: [react(), VitePWA({})],
 });
